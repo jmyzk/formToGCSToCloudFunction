989816f2deb2217c77bfb81d0ba7ea60bdcd1e1b
jmyzk <jmiyazaki@yahoo.co.jp>
2020-06-21 23:13:24 +0900

    #

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def hello_gcs_generic(data, context):
         None; the output is written to Stackdriver Logging
     """
     from google.cloud import storage
-    import sys
-    import io
-    import os
-    import tempfile
+    #import sys
+    #import io
+    #import os
+    #import tempfile
     
     print('Event ID: {}'.format(context.event_id))
     print('Event type: {}'.format(context.event_type))
@@ -23,10 +23,10 @@ def hello_gcs_generic(data, context):
     print('Updated: {}'.format(data['updated']))  
     
     # temp_local_filename = tempfile.mkstemp()
-    storage_client = storage.Client()    
-    bucket_name = data['bucket'] 
-    bucket = storage_client.bucket(bucket_name)
+    #storage_client = storage.Client()    
+    #bucket_name = data['bucket'] 
+    #bucket = storage_client.bucket(bucket_name)
     
-    file = data['name']
+    #file = data['name']
     #blob = bucket.blob(file)
     #blob.download_to_filename(temp_local_filename)
