bba1a9159601abdff98719b83321a5d851106e7e
jmyzk <jmiyazaki@yahoo.co.jp>
2020-06-21 23:20:37 +0900

    revert to sample google code

--- main.py
+++ main.py
@@ -8,25 +8,12 @@ def hello_gcs_generic(data, context):
     Returns:
         None; the output is written to Stackdriver Logging
     """
-    from google.cloud import storage
-    #import sys
-    #import io
-    #import os
-    #import tempfile
-    
+
     print('Event ID: {}'.format(context.event_id))
     print('Event type: {}'.format(context.event_type))
     print('Bucket: {}'.format(data['bucket']))
     print('File: {}'.format(data['name']))
     print('Metageneration: {}'.format(data['metageneration']))
     print('Created: {}'.format(data['timeCreated']))
-    print('Updated: {}'.format(data['updated']))  
-    
-    # temp_local_filename = tempfile.mkstemp()
-    #storage_client = storage.Client()    
-    #bucket_name = data['bucket'] 
-    #bucket = storage_client.bucket(bucket_name)
-    
-    #file = data['name']
-    #blob = bucket.blob(file)
-    #blob.download_to_filename(temp_local_filename)
+    print('Updated: {}'.format(data['updated']))
+
