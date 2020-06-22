86d6a66adab32048deb7d4a3a2e0dee05f38d70b
jmyzk <jmiyazaki@yahoo.co.jp>
2020-06-22 09:20:05 +0900

    clent.bucket('test_bucket')

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ def hello_gcs_generic(data, context):
     Returns:
         None; the output is written to Stackdriver Logging
     """
-    from google.cloud import storage
-    from google.cloud import storage
-    client = storage.Client()
+
 
     print('Event ID: {}'.format(context.event_id))
     print('Event type: {}'.format(context.event_type))
@@ -20,16 +18,9 @@ def hello_gcs_generic(data, context):
     print('Created: {}'.format(data['timeCreated']))
     print('Updated: {}'.format(data['updated']))
     
-    bucket = data['bucket']
-    filename = data['name']
-    filepath = os.path.join(bucket,filename )
-    # Then do other things...
-    blob = bucket.get_blob(filepath)
-    print(blob.download_as_string())
-    blob.upload_from_string('New contents!')
-    filepath = os.path.join(bucket,'storage.txt' )
-    blob2 = bucket.blob(filepath)
-    #blob2.upload_from_filename(filename='/local/path.txt')
+    from google.cloud import storage
+    client = storage.Client()
+    clent.bucket('test_bucket')
 
     
 
